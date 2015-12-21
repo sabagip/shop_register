548d55774fe040b994ea44871e3bae5577a1c941
sergio <sergio@sergio-ThinkPad-L540>
2015-12-21 15:28:53 -0700

    Inicio del todo

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
+
+https://beeznest.wordpress.com/2009/03/16/servicios-web-con-php-utilizando-la-herramienta-nusoap/
 -->
 <?php 
     require_once 'lib/soap/nusoap.php';
